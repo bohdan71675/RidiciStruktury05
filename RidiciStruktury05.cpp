874aa1d448659f60c9d5506ba719667e567bea55
Bohdan Bodnar <bodnarb.04@spst.cz>
2022-10-11 19:10:33 +0200

    Přidejte soubory projektu.

---
+++ RidiciStruktury05.cpp
@@ -0,0 +1,46 @@
+﻿
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int volba;
+    cout << "\n1 = Faktorial, 2 = ?, 3 = Konec: ";
+    cin >> volba;
+    while (volba != 3)
+    {
+        switch (volba)
+        {
+        case 1:
+        {
+            int faktorial = 1;
+            int n;
+            cout << "Zadej cislo n: ";
+            cin >> n;
+            for (int i = n; i > 0 ; i--)
+            {
+                faktorial *= i;
+            }
+            cout << "Faktorial je: " << faktorial << endl;
+
+        }
+        break;
+        case 2:
+        {
+
+        }
+        break;
+        default:
+        {
+            cout << "Nevybrana moznost";
+        }
+        break;
+        }
+        cout << "\n1 = Faktorial, 2 = ?, 3 = Konec: ";
+        cin >> volba;
+    }
+
+
+
+}
